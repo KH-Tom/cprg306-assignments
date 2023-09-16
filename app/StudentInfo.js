d85f626aa7ac60b8f86ade0f89430697e9370da2
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-09-16 14:16:19 -0600

    draft

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Tom Ho</h1>
+      <h1>Student Number: CPRG 306-SA1</h1>
+      <Link href="/">GitHub</Link>
+    </main>
+  );
+}

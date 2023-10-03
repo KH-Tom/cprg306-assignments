d50a87ce937373cb1dcf416d2529824d32f109d2
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-10-03 17:43:23 -0600

    back to home

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,4 +1,5 @@
 import StudentInfo from "../StudentInfo";
+import Link from "next/link";
 
 export default function Week2() {
   return (
@@ -6,6 +7,10 @@ export default function Week2() {
       <h1 className="text-4xl">My Shopping List</h1>
       <p>
         <StudentInfo />
+      </p>
+
+      <p>
+        <Link href="../">Back to Home</Link>
       </p>
     </>
   );
--- app/week3/page.js
+++ app/week3/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ItemList from "./item-list";
+import Link from "next/link";
 
 const Page = () => {
   return (
     <main className="bg-slate-950">
       <h1 className="text-3xl font-bold m-2">Shopping List</h1>
+      <p>
+        <Link href="../">Home</Link>
+      </p>
       <ItemList />
     </main>
   );

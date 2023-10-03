2368a32e864c3cbf525c4df41e5b47dde4e7f840
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-10-03 03:09:28 -0600

    change color

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="bg-slate-50 p-4 border border-gray-200 rounded-md shadow-lg hover:shadow-xl transition-shadow duration-200">
+    <li className="bg-blue-900 p-4 border rounded-md shadow-lg hover:shadow-xl transition-shadow duration-200">
       <div className="flex justify-between items-center mb-2">
-        <span className="text-xl font-semibold text-gray-800">{name}</span>
-        <span className="text-gray-500 px-2 py-1 rounded bg-gray-100">
+        <span className="text-xl font-semibold text-white">{name}</span>
+        <span className="text-white px-2 py-1 rounded bg-gray-100">
           Qty: {quantity}
         </span>
       </div>

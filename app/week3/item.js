d932e8c7b4d97b60f3b658471c74b2c7445abb69
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-10-03 14:08:33 -0600

    update css

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,12 +2,10 @@ import React from "react";
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="bg-blue-900 p-4 border rounded-md shadow-lg hover:shadow-xl transition-shadow duration-200">
+    <li className="p-2 m-4 bg-slate-900 max-w-sm">
       <div className="flex justify-between items-center mb-2">
-        <span className="text-xl font-semibold text-white">{name}</span>
-        <span className="text-white px-2 py-1 rounded bg-gray-100">
-          Qty: {quantity}
-        </span>
+        <span className="ext-xl font-bold">{name}</span>
+        <span className="text-sm">{quantity}</span>
       </div>
       <div className="text-sm mt-1">
         <span className="text-gray-400">{category}</span>
--- app/week3/page.js
+++ app/week3/page.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ItemList from "./item-list";
-import Link from "next/link";
 
 const Page = () => {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-6 text-center">Shopping List</h1>
+    <main className="bg-slate-950">
+      <h1 className="text-3xl font-bold m-2">Shopping List</h1>
       <ItemList />
     </main>
   );

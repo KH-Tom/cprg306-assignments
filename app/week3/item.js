2f7fea5fc4c2e36c0969230de94e746520fc305b
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-10-03 21:28:17 -0600

    redo

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,14 +1,10 @@
-import React from "react";
-
-const Item = ({ name, quantity, category }) => {
+export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-2 m-4 bg-slate-900 max-w-sm">
+    <div className="p-2 m-4 bg-slate-900 max-w-sm">
       <h2 className="text-xl font-bold">{name}</h2>
-      <div className="text-sm">
+      <p className="text-sm">
         Buy {quantity} in {category}
-      </div>
-    </li>
+      </p>
+    </div>
   );
-};
-
-export default Item;
+}
--- app/week3/page.js
+++ app/week3/page.js
@@ -1,17 +1,14 @@
-import React from "react";
 import ItemList from "./item-list";
 import Link from "next/link";
 
-const Page = () => {
+export default function Page() {
   return (
     <main className="bg-slate-950">
-      <h1 className="text-3xl font-bold m-2">Shopping List</h1>
+      <h1 className="text-3xl font-bold m-2">Shopping List_revised</h1>
       <p>
-        <Link href="../">Home</Link>
+        <Link href="../">Back to Home</Link>
       </p>
       <ItemList />
     </main>
   );
-};
-
-export default Page;
+}

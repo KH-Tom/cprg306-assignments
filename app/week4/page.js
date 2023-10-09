9343c2dda29ad81a1098d14e68dffcd858229b46
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-10-08 23:06:24 -0600

    change symbol

--- app/week4/page.js
+++ app/week4/page.js
@@ -4,8 +4,7 @@ import NewItem from "./new-item";
 
 function Page() {
   return (
-    <div className="container mx-auto mt-10">
-      <h1 className="text-2xl mb-6">Add a New Item</h1>
+    <div className="flex justify-center items-start h-screen">
       <NewItem />
     </div>
   );
